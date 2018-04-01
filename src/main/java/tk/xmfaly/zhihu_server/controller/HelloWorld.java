3a6b04af69bb84413a60622da15c3ef73a960cfe
xmfaly <xmfaly@gmail.com>
2018-04-01 18:49:14 +0800

    update

--- src/main/java/tk/xmfaly/zhihu_server/controller/HelloWorld.java
+++ src/main/java/tk/xmfaly/zhihu_server/controller/HelloWorld.java
@@ -2,8 +2,9 @@ package tk.xmfaly.zhihu_server.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping
+@RestController
 public class HelloWorld {
 
     @GetMapping("/")
