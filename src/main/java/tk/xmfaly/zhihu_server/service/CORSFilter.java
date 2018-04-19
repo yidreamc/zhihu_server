53e5ffa051fd71d282e8fa2fa0154a49f3904491
xmfaly <xmfaly@gmail.com>
2018-04-19 20:38:41 +0800

    update

---
+++ src/main/java/tk/xmfaly/zhihu_server/service/CORSFilter.java
@@ -0,0 +1,30 @@
+package tk.xmfaly.zhihu_server.service;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class CORSFilter implements Filter {
+
+
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException, IOException, ServletException, IOException, ServletException {
+
+        HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin","*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "X-Requested-With, accept, content-type, xxxx");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        chain.doFilter(req, res);
+    }
+
+    public void init(FilterConfig filterConfig) {
+    }
+
+    public void destroy() {
+    }
+
+}
